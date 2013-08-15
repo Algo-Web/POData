83581fb5a3e4c04e508e842028aa9a9a7137ce55
drdamour <drdamour@gmail.com>
2013-08-15 01:44:16 -0600

    For #11 added in composer.json

--- Index.php
+++ Index.php
@@ -1,11 +1,8 @@
 <?php
 
 
-require_once dirname(__FILE__).'/library/ODataProducer/Common/ClassAutoLoader.php';
+require_once __DIR__  .'vendor/autoload.php';
 require_once 'Dispatcher.php';
-
-use ODataProducer\Common\ClassAutoLoader;
-ClassAutoLoader::register();
 
 /**
  * Initial entry point for all the request to the library.
