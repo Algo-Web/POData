1498002276295227eab2b4ec3075425802eff140
drdamour <drdamour@gmail.com>
2013-09-18 02:24:37 -0600

    Version code coverage fix

--- tests/UnitTests/POData/Common/VersionTest.php
+++ tests/UnitTests/POData/Common/VersionTest.php
@@ -6,44 +6,57 @@ use POData\Common\Version;
 
 class VersionTest extends \PHPUnit_Framework_TestCase
 {
-    protected function setUp()
-    {
-    }
 
-    public function testUrl()
+    public function testCompareSame()
     {
 
         $version1 = new Version(1, 0);
         $version2 = new Version(1, 0);
-        $this->assertEquals($version1->compare($version2), 0);
+        $this->assertEquals(0, $version1->compare($version2));
+    }
 
+    public function testCompareMajorSameMinorLess(){
         $version1 = new Version(1, 0);
         $version2 = new Version(1, 1);
-        $this->assertEquals($version1->compare($version2), -1);
+        $this->assertEquals(-1, $version1->compare($version2));
+    }
 
+    public function testCompareMajorSameMinorMore(){
+        $version1 = new Version(1, 1);
+        $version2 = new Version(1, 0);
+        $this->assertEquals(1, $version1->compare($version2));
+    }
+
+    public function testCompareMajorLessMinorSame(){
 
         $version1 = new Version(1, 0);
         $version2 = new Version(2, 0);
-        $this->assertEquals($version1->compare($version2), -1);
+        $this->assertEquals(-1, $version1->compare($version2));
+    }
+
+    public function testCompareMajorMoreMinorLess(){
 
         $version1 = new Version(2, 0);
         $version2 = new Version(1, 1);
-        $this->assertEquals($version1->compare($version2), 1);
+        $this->assertEquals(1, $version1->compare($version2));
+    }
 
-        $version1 = new Version(2, 0);
-        $version1->raiseVersion(1, 5);
-        $this->assertEquals($version1->getMajor(), 2);
-        $this->assertEquals($version1->getMinor(), 0);
-        $version1->raiseVersion(3, 0);
-        $this->assertEquals($version1->getMajor(), 3);
-        $this->assertEquals($version1->getMinor(), 0);
-        $version1->raiseVersion(3, 1);
-        $this->assertEquals($version1->getMajor(), 3);
-        $this->assertEquals($version1->getMinor(), 1);
+    public function testRaiseVersion(){
+        $version = new Version(2, 0);
+
+        $version->raiseVersion(1, 5);
+        $this->assertEquals(2, $version->getMajor());
+        $this->assertEquals(0, $version->getMinor());
+
+        $version->raiseVersion(3, 0);
+        $this->assertEquals(3, $version->getMajor());
+        $this->assertEquals(0, $version->getMinor());
+
+        $version->raiseVersion(3, 1);
+        $this->assertEquals(3, $version->getMajor());
+        $this->assertEquals(1, $version->getMinor());
 
     }
 
-    protected function tearDown()
-    {
-    }
+
 }
