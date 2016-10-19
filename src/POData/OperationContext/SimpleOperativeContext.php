5247dd2313af982c24f347c03be8a8c79ebbe139
Chris <c.harris@communicationinnovations.com.au>
2016-10-19 19:09:06 +1000

    Added Some Simple adaptors

---
+++ src/POData/OperationContext/SimpleOperativeContext.php
@@ -0,0 +1,40 @@
+<?php
+namespace POData\OperationContext;
+
+use POData\OperationContext\IHTTPRequest;
+use POData\OperationContext\IOperationContext;
+use POData\OperationContext\Web\OutgoingResponse;
+class SimpleOperationContextAdapter implements IOperationContext
+{
+    /**
+     * @var RequestAdapter;
+     */
+    protected $request;
+    protected $response;
+    /**
+     * @param yii\base\Request $request
+     */
+    public function __construct($request)
+    {
+        $this->request = new RequestAdapter($request);
+        $this->response = new OutgoingResponse();
+    }
+    /**
+     * Gets the Web request context for the request being sent.
+     *
+     * @return OutgoingResponse reference of OutgoingResponse object
+     */
+    public function outgoingResponse()
+    {
+        return $this->response;
+    }
+    /**
+     * Gets the Web request context for the request being received.
+     *
+     * @return IHTTPRequest reference of IncomingRequest object
+     */
+    public function incomingRequest()
+    {
+        return $this->request;
+    }
+}
---
+++ src/POData/OperationContext/SimpleRequestAdapter.php
@@ -0,0 +1,49 @@
+<?php
+namespace POData\OperationContext;
+
+use POData\Common\ODataConstants;
+use POData\OperationContext\HTTPRequestMethod;
+use POData\OperationContext\IHTTPRequest;
+use POData\OperationContext\Web\IncomingRequest;
+class RequestAdapter extends IncomingRequest implements IHTTPRequest
+{
+    protected $request;
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+    /**
+     * get the raw incoming url
+     *
+     * @return string RequestURI called by User with the value of QueryString
+     */
+    public function getRawUrl()
+    {
+        return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' . $_SERVER['REQUEST_URI'];
+    }
+    /**
+     * Returns the Query String Parameters (QSPs) as an array of KEY-VALUE pairs.  If a QSP appears twice
+     * it will have two entries in this array
+     *
+     * @return array[]
+     */
+    public function getQueryParameters()
+    {
+        $data = [];
+        if (is_array($this->request)) {
+            foreach ($this->request as $key => $value) {
+                $data[] = [$key => $value];
+            }
+        }
+        return $data;
+    }
+    /**
+     * Get the HTTP method/verb of the HTTP Request
+     *
+     * @return HTTPRequestMethod
+     */
+    public function getMethod()
+    {
+        return new HTTPRequestMethod($_SERVER['REQUEST_METHOD']);
+    }
+}
