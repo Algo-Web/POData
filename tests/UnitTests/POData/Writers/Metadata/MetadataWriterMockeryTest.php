4c9476377f6b4d7e51856d902772b7e704628a04
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-01-26 00:16:20 +1000

    Default metadata service version to max supported

---
+++ tests/UnitTests/POData/Writers/Metadata/MetadataWriterMockeryTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use POData\Providers\Metadata\ResourceTypeKind;
+use POData\Providers\ProvidersWrapper;
+use POData\Configuration\ServiceConfiguration;
+use POData\Configuration\EntitySetRights;
+use POData\Configuration\ProtocolVersion;
+use POData\Providers\Metadata\IMetadataProvider;
+use POData\Writers\Metadata\MetadataWriter;
+use POData\Common\Version;
+
+use UnitTests\POData\Facets\NorthWind2\NorthWindMetadata;
+use POData\Providers\Query\IQueryProvider;
+
+class MetadataWriterMockeryTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var IQueryProvider */
+    protected $mockQueryProvider;
+
+    protected function setUp()
+    {
+        $this->mockQueryProvider = \Mockery::mock('POData\Providers\Query\IQueryProvider');
+    }
+
+    public function testWriteMetadataInAbsenceOfSpecificVersionRequest()
+    {
+        $northWindMetadata = NorthWindMetadata::Create();
+        $configuration = new ServiceConfiguration($northWindMetadata);
+        $configuration->setEntitySetAccessRule('*', EntitySetRights::ALL);
+        $configuration->setMaxDataServiceVersion(ProtocolVersion::V3());
+
+        $providersWrapper = new ProvidersWrapper(
+            $northWindMetadata, //IMetadataProvider implementation
+            $this->mockQueryProvider, //This should not be used for meta data writing
+            $configuration, //Service configuration
+            false
+        );
+        $metadataWriter = new MetadataWriter($providersWrapper);
+        $metadata = $metadataWriter->writeMetadata();
+
+        $this->assertNotNull($metadata);
+        $this->assertEquals($providersWrapper->getContainerName(), 'NorthWindEntities');
+        $this->assertEquals($providersWrapper->getContainerNamespace(), 'NorthWind');
+
+        $this->assertStringStartsWith('<edmx:Edmx Version="1.0"', $metadata);
+        $versionString = "DataServiceVersion=\"3.0\"";
+        $hasVersionString = false != strpos($metadata, $versionString);
+        $this->assertTrue($hasVersionString);
+
+        $customerResourceSet = $providersWrapper->resolveResourceSet('Customers');
+        $this->assertEquals($customerResourceSet->getName(), 'Customers');
+        $this->assertEquals($customerResourceSet->getResourceType()->getName(), 'Customer');
+
+        $customerEntityType = $providersWrapper->resolveResourceType('Customer');
+        $this->assertEquals($customerEntityType->getResourceTypeKind(), ResourceTypeKind::ENTITY);
+    }
+}
