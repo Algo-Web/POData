ce0b89881f0bf7c3200d9e7596628dd6c43c7852
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-02-04 02:27:06 +1000

    Port ODataWriterRegistry tests

--- tests/UnitTests/POData/Writers/ODataWriterRegistryTest.php
+++ tests/UnitTests/POData/Writers/ODataWriterRegistryTest.php
@@ -6,17 +6,10 @@ use POData\Common\Version;
 use POData\Common\MimeTypes;
 use POData\Writers\ODataWriterRegistry;
 use POData\Writers\IODataWriter;
-use PhockitoUnit\PhockitoUnitTestCase;
-use Phockito\Phockito;
+use Mockery as m;
 
-class ODataWriterRegistryTest extends PhockitoUnitTestCase
+class ODataWriterRegistryTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var IODataWriter */
-    protected $mockWriter1;
-
-    /** @var IODataWriter */
-    protected $mockWriter2;
-
     public function testConstructor()
     {
         $registry = new ODataWriterRegistry();
@@ -26,15 +19,19 @@ class ODataWriterRegistryTest extends PhockitoUnitTestCase
 
     public function testRegister()
     {
+        $writer1 = m::mock(IODataWriter::class)->makePartial();
+        $writer1->shouldReceive('canHandle')->andReturn(false);
+        $writer2 = m::mock(IODataWriter::class)->makePartial();
+        $writer2->shouldReceive('canHandle')->withArgs([Version::v2(), MimeTypes::MIME_APPLICATION_ATOM])
+            ->andReturn(true);
+        $writer2->shouldReceive('canHandle')->andReturn(false);
+
         $registry = new ODataWriterRegistry();
 
-        $registry->register($this->mockWriter1);
-        $registry->register($this->mockWriter2);
+        $registry->register($writer1);
+        $registry->register($writer2);
 
-        Phockito::when($this->mockWriter2->canHandle(Version::v2(), MimeTypes::MIME_APPLICATION_ATOM))
-            ->return(true);
-
-        $this->assertEquals($this->mockWriter2, $registry->getWriter(Version::v2(), MimeTypes::MIME_APPLICATION_ATOM));
+        $this->assertEquals($writer2, $registry->getWriter(Version::v2(), MimeTypes::MIME_APPLICATION_ATOM));
 
         $this->assertNull($registry->getWriter(Version::v1(), MimeTypes::MIME_APPLICATION_ATOM));
 
