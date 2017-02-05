f4b46c4bcc02381c9ecea6c4a60ace75d895df83
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-02-05 13:32:43 +1000

    Move non-phockito tests out of original UriProcessorTest

---
+++ tests/UnitTests/POData/UriProcessor/UriProcessorNewTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UnitTests\POData\UriProcessor;
+
+use Mockery as m;
+
+class UriProcessorNewTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->markTestSkipped('Let\'s see if autoloader picks this up');
+    }
+}
