ad7463ab0ce2aaa337ab36e7c172f741c6388328
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-02-09 02:17:17 +1000

    Cover rest of ODataException

---
+++ tests/UnitTests/POData/Common/ODataExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace UnitTests\POData\Common;
+
+use POData\Common\ODataException;
+
+class ODataExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateForbiddenODataException()
+    {
+        $foo = ODataException::createForbiddenError();
+        $expected = 'Forbidden';
+        $actual = $foo->getMessage();
+        $this->assertEquals($expected, $actual);
+        $this->assertEquals(403, $foo->getStatusCode());
+    }
+
+    public function testCreatePreconditionFailedODataException()
+    {
+        $condition = 'foo == bar';
+        $foo = ODataException::createPreConditionFailedError($condition);
+        $expected = 'foo == bar';
+        $actual = $foo->getMessage();
+        $this->assertEquals($expected, $actual);
+        $this->assertEquals(412, $foo->getStatusCode());
+    }
+
+    public function testCreateUnacceptableValueODataException()
+    {
+        $value = '$i < 0';
+        $foo = ODataException::notAcceptableError($value);
+        $expected = '$i < 0';
+        $actual = $foo->getMessage();
+        $this->assertEquals($expected, $actual);
+        $this->assertEquals(406, $foo->getStatusCode());
+    }
+}
