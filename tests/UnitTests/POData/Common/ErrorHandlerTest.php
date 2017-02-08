ea857492cabc2def650fcffeec28eb6286807665
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-02-09 02:04:16 +1000

    Test bulk of error handler

---
+++ tests/UnitTests/POData/Common/ErrorHandlerTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace UnitTests\POData\Common;
+
+use POData\Common\ErrorHandler;
+use POData\Common\HttpStatus;
+use POData\Common\MimeTypes;
+use POData\Common\ODataConstants;
+use POData\Common\ODataException;
+use Mockery as m;
+use POData\IService;
+use POData\OperationContext\IOperationContext;
+use POData\OperationContext\ServiceHost;
+use POData\OperationContext\Web\OutgoingResponse;
+
+class ErrorHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandleODataException()
+    {
+        $exception = new ODataException("FAIL", "FAIL", 500);
+
+        $outgoing = m::mock(OutgoingResponse::class);
+        $outgoing->shouldReceive('setServiceVersion')
+            ->withArgs([ODataConstants::DATASERVICEVERSION_1_DOT_0 . ';'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStatusCode')->withArgs(['500 Internal Server Error'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setContentType')->withArgs(['application/xml'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStream')->passthru();
+        $outgoing->shouldReceive('getStream')->passthru();
+
+        $context = m::mock(IOperationContext::class);
+        $context->shouldReceive('outgoingResponse')->andReturn($outgoing);
+
+        $host = m::mock(ServiceHost::class)->makePartial();
+        $host->shouldReceive('getRequestAccept')->andReturn([MimeTypes::MIME_APPLICATION_HTTP]);
+        $host->shouldReceive('getOperationContext')->andReturn($context);
+
+
+        $service = m::mock(IService::class);
+        $service->shouldReceive('getHost')->andReturn($host);
+
+        ErrorHandler::handleException($exception, $service);
+
+        $expected = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<error xmlns="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata">
+ <code/>
+ <message>strlen() expects parameter 1 to be string, array given</message>
+</error>
+';
+        $actual = $outgoing->getStream();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testHandleODataExceptionJson()
+    {
+        $exception = new ODataException("FAIL", 500);
+
+        $outgoing = m::mock(OutgoingResponse::class);
+        $outgoing->shouldReceive('setServiceVersion')
+            ->withArgs([ODataConstants::DATASERVICEVERSION_1_DOT_0 . ';'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStatusCode')->withArgs(['500 Internal Server Error'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setContentType')->withArgs(['application/json'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStream')->passthru();
+        $outgoing->shouldReceive('getStream')->passthru();
+
+        $context = m::mock(IOperationContext::class);
+        $context->shouldReceive('outgoingResponse')->andReturn($outgoing);
+
+        $host = m::mock(ServiceHost::class)->makePartial();
+        $host->shouldReceive('getRequestAccept')->andReturn(MimeTypes::MIME_APPLICATION_JSON);
+        $host->shouldReceive('getOperationContext')->andReturn($context);
+
+
+        $service = m::mock(IService::class);
+        $service->shouldReceive('getHost')->andReturn($host);
+
+        ErrorHandler::handleException($exception, $service);
+
+        $expected = '{
+    "error":{
+        "message":{
+            "lang":"en-US","value":"FAIL"
+        }
+    }
+}';
+        $actual = $outgoing->getStream();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testHandleODataExceptionStatusCodeNotModified()
+    {
+        $exception = new ODataException("FAIL", HttpStatus::CODE_NOT_MODIFIED);
+
+        $outgoing = m::mock(OutgoingResponse::class);
+        $outgoing->shouldReceive('setServiceVersion')
+            ->withArgs([ODataConstants::DATASERVICEVERSION_1_DOT_0 . ';'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStatusCode')->withArgs(['304 Not Modified'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setContentType')->withArgs(['application/json'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStream')->passthru();
+        $outgoing->shouldReceive('getStream')->passthru();
+
+        $context = m::mock(IOperationContext::class);
+        $context->shouldReceive('outgoingResponse')->andReturn($outgoing);
+
+        $host = m::mock(ServiceHost::class)->makePartial();
+        $host->shouldReceive('getRequestAccept')->andReturn(MimeTypes::MIME_APPLICATION_JSON);
+        $host->shouldReceive('getOperationContext')->andReturn($context);
+        $host->shouldReceive('setResponseStatusCode')
+            ->withArgs([HttpStatus::CODE_NOT_MODIFIED])->andReturnNull()->once();
+
+
+        $service = m::mock(IService::class);
+        $service->shouldReceive('getHost')->andReturn($host);
+
+        ErrorHandler::handleException($exception, $service);
+    }
+}
