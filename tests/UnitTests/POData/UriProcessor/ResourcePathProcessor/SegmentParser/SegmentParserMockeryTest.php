9320f829d6873928856a8b96399ed806e406f1bc
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-06-21 14:48:33 +1000

    Characterise resource-set misclassification

--- tests/UnitTests/POData/UriProcessor/ResourcePathProcessor/SegmentParser/SegmentParserMockeryTest.php
+++ tests/UnitTests/POData/UriProcessor/ResourcePathProcessor/SegmentParser/SegmentParserMockeryTest.php
@@ -6,7 +6,9 @@ use POData\Common\ODataException;
 use POData\Configuration\EntitySetRights;
 use POData\Configuration\ServiceConfiguration;
 use POData\Providers\Metadata\IMetadataProvider;
+use POData\Providers\Metadata\ResourcePropertyKind;
 use POData\Providers\Metadata\ResourceSetWrapper;
+use POData\Providers\Metadata\ResourceTypeKind;
 use POData\Providers\Metadata\SimpleMetadataProvider;
 use POData\Providers\ProvidersWrapper;
 use POData\Providers\Query\IQueryProvider;
@@ -110,6 +112,35 @@ class SegmentParserMockeryTest extends TestCase
             $actual = $e->getMessage();
         }
         $this->assertEquals($expected, $actual);
+    }
+
+    public function testRelatedResourceSetExpansion()
+    {
+        $foo = NorthWindMetadata::CreateBidirectional();
+        $this->commencePrimaryIgnition($foo);
+
+        $segments = ["Customers(CustomerID='ALFKI', CustomerGuid=guid'15b242e7-52eb-46bd-8f0e-6568b72cd9a6')",
+            'Orders'];
+
+        $result = SegmentParser::parseRequestUriSegments($segments, $this->providersWrapper);
+        $expected = ['Customers', 'Orders'];
+        $actual = [$result[0]->getIdentifier(), $result[1]->getIdentifier()];
+        $this->assertEquals($expected, $actual);
+        $expectedTargetKinds = [TargetKind::RESOURCE(), TargetKind::RESOURCE()];
+        $actualTargetKinds = [$result[0]->getTargetKind(), $result[1]->getTargetKind()];
+        $this->assertEquals($expectedTargetKinds, $actualTargetKinds);
+        $expectedTargetSources = [TargetSource::ENTITY_SET, TargetSource::PROPERTY];
+        $actualTargetSources = [$result[0]->getTargetSource(), $result[1]->getTargetSource()];
+        $this->assertEquals($expectedTargetSources, $actualTargetSources);
+        // As we're expanding the many side of a one-to-many relation, first segment should be a singleton,
+        // second segment should not
+        $expectedSingles = [true, false];
+        $actualSingles = [$result[0]->isSingleResult(), $result[1]->isSingleResult()];
+        $this->assertEquals($expectedSingles, $actualSingles);
+        $this->assertNull($result[0]->getProjectedProperty());
+        $projected = $result[1]->getProjectedProperty();
+        $this->assertNotNull($projected);
+        $this->assertEquals(ResourcePropertyKind::RESOURCESET_REFERENCE, $projected->getTypeKind());
     }
 
     public static function exampleSingleton()
