bddc69594e7789a093afe3339d53c23bc8b0ee53
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-07-02 00:37:28 +1000

    Strip out html-expansion debris from Illuminate-request query parameters
    For example, &amp;$skipToken= is being exploded, trimming the leading &,
    but leaving the rest of the html expansion in place.  This causes problems
    further inside the library, which is expecting $skipToken => 'foo', not
    the debris-encrusted version.

---
+++ tests/UnitTests/POData/OperationContext/Web/IncomingIlluminateRequestTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace UnitTests\POData\OperationContext\Web;
+
+use Illuminate\Http\Request;
+use POData\OperationContext\Web\Illuminate\IncomingIlluminateRequest;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use UnitTests\POData\TestCase;
+use Mockery as m;
+
+class IncomingIlluminateRequestTest extends TestCase
+{
+    public function testHandlingHtmlExpansionDebrisInParmNames()
+    {
+        $rawParm = [
+            '$orderBy' => 'CustomerTitle desc,id desc',
+            'amp;$top' => '1',
+            'amp;$skipToken' => "'University+of+Loamshire', 1, 1"
+        ];
+
+        $expectedParm = [
+            [ '$orderBy' => 'CustomerTitle desc,id desc'],
+            [ '$top' => '1'],
+            [ '$skipToken' => "'University+of+Loamshire', 1, 1"]
+        ];
+
+        $request = new Request($rawParm, $rawParm);
+        $request->setMethod('GET');
+
+        $foo = new IncomingIlluminateRequest($request);
+
+        $actualParm = $foo->getQueryParameters();
+        $this->assertEquals($expectedParm, $actualParm);
+    }
+
+    public function testFalseRequestHeaderReturnsNull()
+    {
+        $request = m::mock(Request::class);
+        $request->shouldReceive('header')->withAnyArgs()->andReturn(false);
+        $request->shouldReceive('getMethod')->andReturn('GET');
+
+        $foo = new IncomingIlluminateRequest($request);
+        $this->assertNull($foo->getRequestHeader('header'));
+    }
+
+    public function testEmptyRequestHeaderReturnsNull()
+    {
+        $request = m::mock(Request::class);
+        $request->shouldReceive('header')->withAnyArgs()->andReturn("");
+        $request->shouldReceive('getMethod')->andReturn('GET');
+
+        $foo = new IncomingIlluminateRequest($request);
+        $this->assertNull($foo->getRequestHeader('header'));
+    }
+
+    public function testNullRequestHeaderReturnsNull()
+    {
+        $request = m::mock(Request::class);
+        $request->shouldReceive('header')->withAnyArgs()->andReturn(null);
+        $request->shouldReceive('getMethod')->andReturn('GET');
+
+        $foo = new IncomingIlluminateRequest($request);
+        $this->assertNull($foo->getRequestHeader('header'));
+    }
+}
