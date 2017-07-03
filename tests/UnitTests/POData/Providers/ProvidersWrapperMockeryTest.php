18ce713ebd65c968ccdfba08fa60c1ea7543f9d6
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-07-04 02:43:42 +1000

    Add tests missed in previous pull req

--- tests/UnitTests/POData/Providers/ProvidersWrapperMockeryTest.php
+++ tests/UnitTests/POData/Providers/ProvidersWrapperMockeryTest.php
@@ -4,9 +4,14 @@ namespace UnitTests\POData\Providers\Metadata;
 
 use POData\Configuration\IServiceConfiguration;
 use POData\Providers\Metadata\ResourceFunctionType;
+use POData\Providers\Metadata\ResourceProperty;
+use POData\Providers\Metadata\ResourceSet;
 use POData\Providers\Metadata\SimpleMetadataProvider;
+use POData\Providers\ProvidersQueryWrapper;
 use POData\Providers\ProvidersWrapper;
 use POData\Providers\Query\IQueryProvider;
+use POData\Providers\Query\QueryType;
+use POData\UriProcessor\ResourcePathProcessor\SegmentParser\KeyDescriptor;
 use UnitTests\POData\TestCase;
 use Mockery as m;
 
@@ -171,6 +176,151 @@ class ProvidersWrapperMockeryTest extends TestCase
         $result = $foo->getSingletons();
         $this->assertTrue(is_array($result));
         $this->assertEquals(0, count($result));
+    }
+
+    public function testGetResourceFromResourceSet()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('getResourceFromResourceSet')->andReturn(null)->once();
+
+        $set = m::mock(ResourceSet::class);
+        $key = m::mock(KeyDescriptor::class);
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull($foo->getResourceFromResourceSet($set, $key));
+    }
+
+    public function testPutResource()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('putResource')->andReturn(null)->once();
+
+        $set = m::mock(ResourceSet::class);
+        $key = m::mock(KeyDescriptor::class);
+        $data = [];
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull($foo->putResource($set, $key, $data));
+    }
+
+    public function testGetRelatedResourceSet()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('getRelatedResourceSet')->andReturn(null)->once();
+
+        $type = QueryType::ENTITIES();
+
+        $set = m::mock(ResourceSet::class);
+        $key = m::mock(KeyDescriptor::class);
+        $property = m::mock(ResourceProperty::class);
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull(
+            $foo->getRelatedResourceSet($type, $set, new \stdClass(), $set, $property, null, null, null, null)
+        );
+    }
+
+    public function testGetResourceFromRelatedResourceSet()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('getResourceFromRelatedResourceSet')->andReturn(null)->once();
+
+        $set = m::mock(ResourceSet::class);
+        $key = m::mock(KeyDescriptor::class);
+        $property = m::mock(ResourceProperty::class);
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull(
+            $foo->getResourceFromRelatedResourceSet($set, new \stdClass(), $set, $property, $key)
+        );
+    }
+
+    public function testGetRelatedResourceReference()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('getRelatedResourceReference')->andReturn(null)->once();
+
+        $set = m::mock(ResourceSet::class);
+        $property = m::mock(ResourceProperty::class);
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull(
+            $foo->getRelatedResourceReference($set, new \stdClass(), $set, $property)
+        );
+    }
+
+    public function testUpdateResource()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('updateResource')->andReturn(null)->once();
+
+        $set = m::mock(ResourceSet::class);
+        $key = m::mock(KeyDescriptor::class);
+        $property = m::mock(ResourceProperty::class);
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull(
+            $foo->updateResource($set, new \stdClass(), $key, true)
+        );
+    }
+
+    public function testDeleteResource()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('deleteResource')->andReturn(null)->once();
+
+        $set = m::mock(ResourceSet::class);
+        $key = m::mock(KeyDescriptor::class);
+        $property = m::mock(ResourceProperty::class);
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull(
+            $foo->deleteResource($set, new \stdClass())
+        );
+    }
+
+    public function testCreateResourceforResourceSet()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('createResourceforResourceSet')->andReturn(null)->once();
+
+        $set = m::mock(ResourceSet::class);
+        $key = m::mock(KeyDescriptor::class);
+        $property = m::mock(ResourceProperty::class);
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertNull(
+            $foo->createResourceforResourceSet($set, new \stdClass(), [])
+        );
+    }
+
+    public function testHandlesOrderedPaging()
+    {
+        $query = m::mock(ProvidersQueryWrapper::class);
+        $query->shouldReceive('handlesOrderedPaging')->andReturn(true)->once();
+
+        $foo = m::mock(ProvidersWrapper::class)->makePartial();
+        $foo->shouldReceive('getProviderWrapper')->andReturn($query);
+
+        $this->assertTrue(
+            $foo->handlesOrderedPaging()
+        );
     }
 
     public static function mockProperty($object, $propertyName, $value)
