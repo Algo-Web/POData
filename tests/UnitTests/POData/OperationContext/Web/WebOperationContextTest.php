64ae82e222486268bb1183ceedef5328f6022f2b
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-07-14 18:46:34 +1000

    Cover OutgoingResponse & WebOperationContext

--- tests/UnitTests/POData/OperationContext/Web/WebOperationContextTest.php
+++ tests/UnitTests/POData/OperationContext/Web/WebOperationContextTest.php
@@ -1,14 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 14/07/17
- * Time: 6:13 PM
- */
 
 namespace UnitTests\POData\OperationContext\Web;
 
-class WebOperationContextTest
-{
+use POData\OperationContext\IHTTPRequest;
+use POData\OperationContext\Web\OutgoingResponse;
+use POData\OperationContext\Web\WebOperationContext;
+use UnitTests\POData\TestCase;
 
+class WebOperationContextTest extends TestCase
+{
+    public function testWebOperationContextCtor()
+    {
+        $foo = new WebOperationContext();
+        $this->assertTrue($foo->incomingRequest() instanceof IHTTPRequest);
+        $this->assertTrue($foo->outgoingResponse() instanceof OutgoingResponse);
+    }
 }
