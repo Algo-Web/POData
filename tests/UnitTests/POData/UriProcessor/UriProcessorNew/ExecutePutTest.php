96509ef2f918f4406642c190fdbd48135da99a92
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-07-30 00:30:25 +1000

    Promote UriProcessorNew PUT tests

--- tests/UnitTests/POData/UriProcessor/UriProcessorNew/ExecutePutTest.php
+++ tests/UnitTests/POData/UriProcessor/UriProcessorNew/ExecutePutTest.php
@@ -4,6 +4,7 @@ namespace UnitTests\POData\UriProcessor\UriProcessorNew;
 
 use Mockery as m;
 use POData\Common\ODataConstants;
+use POData\Common\ODataException;
 use POData\Common\Url;
 use POData\Common\Version;
 use POData\Configuration\IServiceConfiguration;
@@ -12,22 +13,13 @@ use POData\OperationContext\HTTPRequestMethod;
 use POData\OperationContext\IHTTPRequest;
 use POData\OperationContext\IOperationContext;
 use POData\OperationContext\ServiceHost;
-use POData\Providers\Metadata\ResourceComplexType;
-use POData\Providers\Metadata\ResourceEntityType;
-use POData\Providers\Metadata\ResourceFunctionType;
-use POData\Providers\Metadata\ResourcePrimitiveType;
 use POData\Providers\Metadata\ResourceProperty;
-use POData\Providers\Metadata\ResourcePropertyKind;
-use POData\Providers\Metadata\ResourceSet;
 use POData\Providers\Metadata\ResourceSetWrapper;
-use POData\Providers\Metadata\ResourceStreamInfo;
 use POData\Providers\Metadata\ResourceType;
 use POData\Providers\Metadata\ResourceTypeKind;
-use POData\Providers\Metadata\Type\Int32;
 use POData\Providers\Metadata\Type\IType;
 use POData\Providers\ProvidersWrapper;
-use POData\UriProcessor\RequestDescription;
-use POData\UriProcessor\UriProcessor;
+use POData\UriProcessor\ResourcePathProcessor\SegmentParser\SegmentDescriptor;
 use POData\UriProcessor\UriProcessorNew;
 use UnitTests\POData\TestCase;
 
@@ -44,7 +36,7 @@ class ExecutePutTest extends TestCase
         $host->shouldReceive('getRequestVersion')->andReturn('1.0');
         $host->shouldReceive('getRequestMaxVersion')->andReturn('3.0');
         $host->shouldReceive('getQueryStringItem')->andReturn(null);
-        $host->shouldReceive('getRequestContentType')->andReturn(ODataConstants::FORMAT_ATOM)->atLeast(2);
+        $host->shouldReceive('getRequestContentType')->andReturn(ODataConstants::FORMAT_ATOM)->atLeast(1);
 
         $request = m::mock(IHTTPRequest::class);
         $request->shouldReceive('getMethod')->andReturn(HTTPRequestMethod::PUT());
@@ -56,14 +48,14 @@ class ExecutePutTest extends TestCase
         $resourceType = m::mock(ResourceType::class);
         $resourceType->shouldReceive('getName')->andReturn('Customer');
         $resourceType->shouldReceive('getResourceTypeKind')->andReturn(ResourceTypeKind::ENTITY());
-        $resourceType->shouldReceive('getKeyProperties')->andReturn([])->atLeast(2);
-        $resourceType->shouldReceive('getInstanceType->newInstance')->andReturn(new \stdClass())->atLeast(2);
+        $resourceType->shouldReceive('getKeyProperties')->andReturn([])->atLeast(1);
+        $resourceType->shouldReceive('getInstanceType->newInstance')->andReturn(new \stdClass())->atLeast(1);
 
         $result = ['eins', 'zwei', 'polizei'];
 
         $resourceSet = m::mock(ResourceSetWrapper::class);
         $resourceSet->shouldReceive('getResourceType')->andReturn($resourceType);
-        $resourceSet->shouldReceive('checkResourceSetRightsForRead')->andReturnNull()->atLeast(2);
+        $resourceSet->shouldReceive('checkResourceSetRightsForRead')->andReturnNull()->atLeast(1);
         $resourceSet->shouldReceive('hasNamedStreams')->andReturn(false);
         $resourceSet->shouldReceive('hasBagProperty')->andReturn(false);
         $resourceSet->shouldReceive('getResourceSetPageSize')->andReturn(200);
@@ -82,20 +74,13 @@ class ExecutePutTest extends TestCase
         $service->shouldReceive('getOperationContext')->andReturn($context);
         $service->shouldReceive('getConfiguration')->andReturn($config);
 
-        $original = UriProcessor::process($service);
         $remix = UriProcessorNew::process($service);
 
-        $expected = null;
-        $expectedClass = null;
+        $expected = 'The URI \'http://localhost/odata.svc/customers\' is not valid for PUT method.';
+        $expectedClass = ODataException::class;
         $actual = null;
         $actualClass = null;
 
-        try {
-            $original->execute();
-        } catch (\Exception $e) {
-            $expectedClass = get_class($e);
-            $expected = $e->getMessage();
-        }
         try {
             $remix->execute();
         } catch (\Exception $e) {
@@ -117,7 +102,7 @@ class ExecutePutTest extends TestCase
         $host->shouldReceive('getRequestVersion')->andReturn('1.0');
         $host->shouldReceive('getRequestMaxVersion')->andReturn('3.0');
         $host->shouldReceive('getQueryStringItem')->andReturn(null);
-        $host->shouldReceive('getRequestContentType')->andReturn(ODataConstants::FORMAT_ATOM)->atLeast(2);
+        $host->shouldReceive('getRequestContentType')->andReturn(ODataConstants::FORMAT_ATOM)->atLeast(1);
 
         $request = m::mock(IHTTPRequest::class);
         $request->shouldReceive('getMethod')->andReturn(HTTPRequestMethod::PUT());
@@ -127,7 +112,7 @@ class ExecutePutTest extends TestCase
         $context->shouldReceive('incomingRequest')->andReturn($request);
 
         $iType = m::mock(IType::class);
-        $iType->shouldReceive('isCompatibleWith')->andReturn(true)->atLeast(2);
+        $iType->shouldReceive('isCompatibleWith')->andReturn(true)->atLeast(1);
 
         $keyProp = m::mock(ResourceProperty::class);
         $keyProp->shouldReceive('getInstanceType')->andReturn($iType);
@@ -135,14 +120,14 @@ class ExecutePutTest extends TestCase
         $resourceType = m::mock(ResourceType::class);
         $resourceType->shouldReceive('getName')->andReturn('Customer');
         $resourceType->shouldReceive('getResourceTypeKind')->andReturn(ResourceTypeKind::ENTITY());
-        $resourceType->shouldReceive('getKeyProperties')->andReturn(['id' => $keyProp])->atLeast(2);
-        $resourceType->shouldReceive('getInstanceType->newInstance')->andReturn(new \stdClass())->atLeast(2);
+        $resourceType->shouldReceive('getKeyProperties')->andReturn(['id' => $keyProp])->atLeast(1);
+        $resourceType->shouldReceive('getInstanceType->newInstance')->andReturn(new \stdClass())->atLeast(1);
 
         $result = 'eins';
 
         $resourceSet = m::mock(ResourceSetWrapper::class);
         $resourceSet->shouldReceive('getResourceType')->andReturn($resourceType);
-        $resourceSet->shouldReceive('checkResourceSetRightsForRead')->andReturnNull()->atLeast(2);
+        $resourceSet->shouldReceive('checkResourceSetRightsForRead')->andReturnNull()->atLeast(1);
         $resourceSet->shouldReceive('hasNamedStreams')->andReturn(false);
         $resourceSet->shouldReceive('hasBagProperty')->andReturn(false);
         $resourceSet->shouldReceive('getResourceSetPageSize')->andReturn(200);
@@ -150,7 +135,7 @@ class ExecutePutTest extends TestCase
         $wrapper = m::mock(ProvidersWrapper::class);
         $wrapper->shouldReceive('resolveSingleton')->andReturn(null);
         $wrapper->shouldReceive('resolveResourceSet')->andReturn($resourceSet);
-        $wrapper->shouldReceive('getResourceFromResourceSet')->andReturn($result)->twice();
+        $wrapper->shouldReceive('getResourceFromResourceSet')->andReturn($result)->once();
         $wrapper->shouldReceive('updateResource')->withAnyArgs()->andReturnNull()->never();
 
         $config = m::mock(IServiceConfiguration::class);
@@ -162,20 +147,13 @@ class ExecutePutTest extends TestCase
         $service->shouldReceive('getOperationContext')->andReturn($context);
         $service->shouldReceive('getConfiguration')->andReturn($config);
 
-        $original = UriProcessor::process($service);
         $remix = UriProcessorNew::process($service);
 
-        $expected = null;
-        $expectedClass = null;
+        $expected = 'Method PUT expecting some data, but received empty data.';
+        $expectedClass = ODataException::class;
         $actual = null;
         $actualClass = null;
 
-        try {
-            $original->execute();
-        } catch (\Exception $e) {
-            $expectedClass = get_class($e);
-            $expected = $e->getMessage();
-        }
         try {
             $remix->execute();
         } catch (\Exception $e) {
@@ -230,9 +208,9 @@ class ExecutePutTest extends TestCase
         $wrapper = m::mock(ProvidersWrapper::class);
         $wrapper->shouldReceive('resolveSingleton')->andReturn(null);
         $wrapper->shouldReceive('resolveResourceSet')->andReturn($resourceSet);
-        $wrapper->shouldReceive('getResourceFromResourceSet')->andReturn($result)->twice();
+        $wrapper->shouldReceive('getResourceFromResourceSet')->andReturn($result)->once();
         $wrapper->shouldReceive('updateResource')->with($resourceSet, 'eins', m::any(), ['a', 'b', 'c'], false)
-            ->andReturn('zwei')->twice();
+            ->andReturn('zwei')->once();
 
         $config = m::mock(IServiceConfiguration::class);
         $config->shouldReceive('getMaxDataServiceVersion')->andReturn(new Version(3, 0));
@@ -243,15 +221,13 @@ class ExecutePutTest extends TestCase
         $service->shouldReceive('getOperationContext')->andReturn($context);
         $service->shouldReceive('getConfiguration')->andReturn($config);
 
-        $original = UriProcessor::process($service);
         $remix = UriProcessorNew::process($service);
 
-        $original->execute();
         $remix->execute();
 
-        $origSegment = $original->getRequest()->getLastSegment();
+        $origSegment = new SegmentDescriptor();
+        $origSegment->setResult('zwei');
         $remixSegment = $remix->getRequest()->getLastSegment();
-        $this->assertEquals('zwei', $origSegment->getResult());
-        $this->assertEquals('zwei', $remixSegment->getResult());
+        $this->assertEquals($origSegment->getResult(), $remixSegment->getResult());
     }
 }
