3259da3c325586b2f26bad806723fe328f4559ea
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-07-30 00:13:38 +1000

    Promote UriProcessorNew POST tests

--- tests/UnitTests/POData/UriProcessor/UriProcessorNew/ExecutePostTest.php
+++ tests/UnitTests/POData/UriProcessor/UriProcessorNew/ExecutePostTest.php
@@ -4,6 +4,7 @@ namespace UnitTests\POData\UriProcessor\UriProcessorNew;
 
 use Mockery as m;
 use POData\Common\ODataConstants;
+use POData\Common\ODataException;
 use POData\Common\Url;
 use POData\Common\Version;
 use POData\Configuration\IServiceConfiguration;
@@ -12,22 +13,13 @@ use POData\OperationContext\HTTPRequestMethod;
 use POData\OperationContext\IHTTPRequest;
 use POData\OperationContext\IOperationContext;
 use POData\OperationContext\ServiceHost;
-use POData\Providers\Metadata\ResourceComplexType;
-use POData\Providers\Metadata\ResourceEntityType;
-use POData\Providers\Metadata\ResourceFunctionType;
-use POData\Providers\Metadata\ResourcePrimitiveType;
 use POData\Providers\Metadata\ResourceProperty;
-use POData\Providers\Metadata\ResourcePropertyKind;
-use POData\Providers\Metadata\ResourceSet;
 use POData\Providers\Metadata\ResourceSetWrapper;
-use POData\Providers\Metadata\ResourceStreamInfo;
 use POData\Providers\Metadata\ResourceType;
 use POData\Providers\Metadata\ResourceTypeKind;
-use POData\Providers\Metadata\Type\Int32;
 use POData\Providers\Metadata\Type\IType;
 use POData\Providers\ProvidersWrapper;
-use POData\UriProcessor\RequestDescription;
-use POData\UriProcessor\UriProcessor;
+use POData\UriProcessor\ResourcePathProcessor\SegmentParser\SegmentDescriptor;
 use POData\UriProcessor\UriProcessorNew;
 use UnitTests\POData\TestCase;
 
@@ -89,20 +81,13 @@ class ExecutePostTest extends TestCase
         $service->shouldReceive('getOperationContext')->andReturn($context);
         $service->shouldReceive('getConfiguration')->andReturn($config);
 
-        $original = UriProcessor::process($service);
         $remix = UriProcessorNew::process($service);
 
-        $expected = null;
-        $expectedClass = null;
+        $expected = 'Method POST expecting some data, but received empty data.';
+        $expectedClass = ODataException::class;
         $actual = null;
         $actualClass = null;
 
-        try {
-            $original->execute();
-        } catch (\Exception $e) {
-            $expectedClass = get_class($e);
-            $expected = $e->getMessage();
-        }
         try {
             $remix->execute();
         } catch (\Exception $e) {
@@ -124,7 +109,7 @@ class ExecutePostTest extends TestCase
         $host->shouldReceive('getRequestVersion')->andReturn('1.0');
         $host->shouldReceive('getRequestMaxVersion')->andReturn('3.0');
         $host->shouldReceive('getQueryStringItem')->andReturn(null);
-        $host->shouldReceive('getRequestContentType')->andReturn(ODataConstants::FORMAT_ATOM)->atLeast(2);
+        $host->shouldReceive('getRequestContentType')->andReturn(ODataConstants::FORMAT_ATOM)->atLeast(1);
 
         $request = m::mock(IHTTPRequest::class);
         $request->shouldReceive('getMethod')->andReturn(HTTPRequestMethod::POST());
@@ -134,7 +119,7 @@ class ExecutePostTest extends TestCase
         $context->shouldReceive('incomingRequest')->andReturn($request);
 
         $iType = m::mock(IType::class);
-        $iType->shouldReceive('isCompatibleWith')->andReturn(true)->atLeast(2);
+        $iType->shouldReceive('isCompatibleWith')->andReturn(true)->atLeast(1);
 
         $keyProp = m::mock(ResourceProperty::class);
         $keyProp->shouldReceive('getInstanceType')->andReturn($iType);
@@ -142,14 +127,14 @@ class ExecutePostTest extends TestCase
         $resourceType = m::mock(ResourceType::class);
         $resourceType->shouldReceive('getName')->andReturn('Customer');
         $resourceType->shouldReceive('getResourceTypeKind')->andReturn(ResourceTypeKind::ENTITY());
-        $resourceType->shouldReceive('getKeyProperties')->andReturn(['id' => $keyProp])->atLeast(2);
-        $resourceType->shouldReceive('getInstanceType->newInstance')->andReturn(new \stdClass())->atLeast(2);
+        $resourceType->shouldReceive('getKeyProperties')->andReturn(['id' => $keyProp])->atLeast(1);
+        $resourceType->shouldReceive('getInstanceType->newInstance')->andReturn(new \stdClass())->atLeast(1);
 
         $result = 'eins';
 
         $resourceSet = m::mock(ResourceSetWrapper::class);
         $resourceSet->shouldReceive('getResourceType')->andReturn($resourceType);
-        $resourceSet->shouldReceive('checkResourceSetRightsForRead')->andReturnNull()->atLeast(2);
+        $resourceSet->shouldReceive('checkResourceSetRightsForRead')->andReturnNull()->atLeast(1);
         $resourceSet->shouldReceive('hasNamedStreams')->andReturn(false);
         $resourceSet->shouldReceive('hasBagProperty')->andReturn(false);
         $resourceSet->shouldReceive('getResourceSetPageSize')->andReturn(200);
@@ -159,7 +144,7 @@ class ExecutePostTest extends TestCase
         $wrapper->shouldReceive('resolveResourceSet')->andReturn($resourceSet);
         $wrapper->shouldReceive('getResourceFromResourceSet')->andReturn($result)->never();
         $wrapper->shouldReceive('createResourceforResourceSet')->with($resourceSet, m::any(), ['a', 'b', 'c'])
-            ->andReturn('polizei')->twice();
+            ->andReturn('polizei')->once();
 
         $config = m::mock(IServiceConfiguration::class);
         $config->shouldReceive('getMaxDataServiceVersion')->andReturn(new Version(3, 0));
@@ -170,15 +155,13 @@ class ExecutePostTest extends TestCase
         $service->shouldReceive('getOperationContext')->andReturn($context);
         $service->shouldReceive('getConfiguration')->andReturn($config);
 
-        $original = UriProcessor::process($service);
         $remix = UriProcessorNew::process($service);
 
-        $original->execute();
         $remix->execute();
 
-        $origSegment = $original->getRequest()->getLastSegment();
+        $origSegment = new SegmentDescriptor();
+        $origSegment->setResult('polizei');
         $remixSegment = $remix->getRequest()->getLastSegment();
-        $this->assertEquals('polizei', $origSegment->getResult());
-        $this->assertEquals('polizei', $remixSegment->getResult());
+        $this->assertEquals($origSegment->getResult(), $remixSegment->getResult());
     }
 }
