58c00d3fffaf25b406e26d24b60cb2e526563955
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-12-09 01:06:42 +1000

    Fix spelling errors in ChangeSetParser

--- src/POData/BatchProcessor/ChangeSetParser.php
+++ src/POData/BatchProcessor/ChangeSetParser.php
@@ -9,7 +9,7 @@ use POData\OperationContext\Web\Illuminate\IlluminateOperationContext;
 class ChangeSetParser implements IBatchParser
 {
     protected $data;
-    protected $changeSetBoundtry;
+    protected $changeSetBoundary;
     protected $rawRequests = [];
     protected $service;
     protected $ContentIDToLocationLookup =[];
@@ -18,86 +18,7 @@ class ChangeSetParser implements IBatchParser
     {
         $this->service = $service;
         $this->data = trim($body);
-        $firstLine = trim(strtok($this->data, "\n"));
-        $this->changeSetBoundtry = substr($firstLine, 40);
-
-        $prefix = 'HTTP_';
-        $matches = explode('--'.$this->changeSetBoundtry, $this->data);
-        array_shift($matches);
-        $contentIDinit = -1;
-        foreach ($matches as $match) {
-            if ('--' === trim($match)) {
-                continue;
-            }
-            $stage = 0;
-            $gotRequestPathParts = false;
-            $match = trim($match);
-            $lines = explode("\n", $match);
-
-            $RequestPathParts = [];
-            $serverParts = [];
-            $contentID = $contentIDinit;
-            $content = '';
-            foreach ($lines as $line) {
-                if ('' == $line) {
-                    $stage++;
-                    continue;
-                }
-                switch ($stage) {
-                    case 0:
-                        if (strtolower('Content-Type') == strtolower(substr($line, 0, 12))
-                            && 'application/http' != strtolower(substr($line, -16))) {
-                            //TODO: throw an error about incorrect content type for changeSet
-                        }
-                        if (strtolower('Content-Transfer-Encoding') == strtolower(substr($line, 0, 25))
-                            && 'binary' != strtolower(substr($line, -6))) {
-                            //TODO: throw an error about unsupported encoding
-                        }
-                        break;
-                    case 1:
-                        if (!$gotRequestPathParts) {
-                            $RequestPathParts = explode(' ', $line);
-                            $gotRequestPathParts = true;
-                            continue;
-                        }
-                        $headerSides = explode(':', $line);
-                        if (count($headerSides) != 2) {
-                            //TODO: we should throw an error her
-                            dd($line);
-                        }
-                        if (strtolower(trim($headerSides[0])) == strtolower('Content-ID')) {
-                            $contentID = trim($headerSides[1]);
-                            continue;
-                        }
-
-                        $name = trim($headerSides[0]);
-                        $name = strtr(strtoupper($name), '-', '_');
-                        $value = trim($headerSides[1]);
-                        if (! starts_with($name, $prefix) && $name != 'CONTENT_TYPE') {
-                            $name = $prefix.$name;
-                        }
-                        $serverParts[$name] = $value;
-
-                        break;
-                    case 2:
-                        $content .= $line;
-                        break;
-                    default:
-                        throw new \Exception('how did we end up with more then 3 stages??');
-                }
-            }
-            if ($contentIDinit == $contentID) {
-                $contentIDinit--;
-            }
-            $this->rawRequests[$contentID] = (object)['RequestVerb'  => $RequestPathParts[0],
-                                                      'RequestURL'   => $RequestPathParts[1],
-                                                      'ServerParams' => $serverParts,
-                                                      'Content'      => $content,
-                                                      'Request'      => null,
-                                                      'Response'     => null];
-        }
     }
-
 
     public function process()
     {
@@ -129,25 +50,129 @@ class ChangeSetParser implements IBatchParser
             }
         }
     }
-    public function getResponse(){
+
+    public function getResponse()
+    {
         $response = "";
-        foreach ($this->rawRequests as $contentID => $workibngObject) {
-            $response .= $this->changeSetBoundtry === false ? "" : '--' . $this->changeSetBoundtry . "\r\n";
+        foreach ($this->rawRequests as $contentID => $workingObject) {
+            $response .= false === $this->changeSetBoundary ? "" : '--' . $this->changeSetBoundary . "\r\n";
             $response .= 'Content-Type: application/http' . "\r\n";
             $response .= 'Content-Transfer-Encoding: binary' . "\r\n";
             $response .= "\r\n";
-            $headers = $workibngObject->Response->getHeaders();
+            $headers = $workingObject->Response->getHeaders();
             foreach ($headers as $headerName => $headerValue) {
                 if (null !== $headerValue) {
                      $response .= $headerName . ':' . $headerValue . "\r\n";
                 }
             }
             $response .= "\r\n";
-            $response .= $workibngObject->Response->getStream();
+            $response .= $workingObject->Response->getStream();
 
 
         }
-        $response .= $this->changeSetBoundtry === false ? "" : '--' . $this->changeSetBoundtry . '--' . "\r\n";
+        $response .= false === $this->changeSetBoundary ? "" : '--' . $this->changeSetBoundary . '--' . "\r\n";
         return $response;
+    }
+
+    public function handleData()
+    {
+        $firstLine = trim(strtok($this->getData(), "\n"));
+        $this->changeSetBoundary = substr($firstLine, 40);
+
+        $prefix = 'HTTP_';
+        $matches = explode('--' . $this->changeSetBoundary, $this->getData());
+        array_shift($matches);
+        $contentIDinit = -1;
+        foreach ($matches as $match) {
+            if ('--' === trim($match)) {
+                continue;
+            }
+            $stage = 0;
+            $gotRequestPathParts = false;
+            $match = trim($match);
+            $lines = explode("\n", $match);
+
+            $RequestPathParts = [];
+            $serverParts = [];
+            $contentID = $contentIDinit;
+            $content = '';
+            foreach ($lines as $line) {
+                if ('' == $line) {
+                    $stage++;
+                    continue;
+                }
+                switch ($stage) {
+                    case 0:
+                        if (strtolower('Content-Type') == strtolower(substr($line, 0, 12))
+                            && 'application/http' != strtolower(substr($line, -16))
+                        ) {
+                            //TODO: throw an error about incorrect content type for changeSet
+                        }
+                        if (strtolower('Content-Transfer-Encoding') == strtolower(substr($line, 0, 25))
+                            && 'binary' != strtolower(substr($line, -6))
+                        ) {
+                            //TODO: throw an error about unsupported encoding
+                        }
+                        break;
+                    case 1:
+                        if (!$gotRequestPathParts) {
+                            $RequestPathParts = explode(' ', $line);
+                            $gotRequestPathParts = true;
+                            continue;
+                        }
+                        $headerSides = explode(':', $line);
+                        if (count($headerSides) != 2) {
+                            //TODO: we should throw an error here
+                            dd($line);
+                        }
+                        if (strtolower(trim($headerSides[0])) == strtolower('Content-ID')) {
+                            $contentID = trim($headerSides[1]);
+                            continue;
+                        }
+
+                        $name = trim($headerSides[0]);
+                        $name = strtr(strtoupper($name), '-', '_');
+                        $value = trim($headerSides[1]);
+                        if (!starts_with($name, $prefix) && $name != 'CONTENT_TYPE') {
+                            $name = $prefix . $name;
+                        }
+                        $serverParts[$name] = $value;
+
+                        break;
+                    case 2:
+                        $content .= $line;
+                        break;
+                    default:
+                        throw new \Exception('how did we end up with more than 3 stages??');
+                }
+            }
+            if ($contentIDinit == $contentID) {
+                $contentIDinit--;
+            }
+            $this->rawRequests[$contentID] = (object)[
+                'RequestVerb' => $RequestPathParts[0],
+                'RequestURL' => $RequestPathParts[1],
+                'ServerParams' => $serverParts,
+                'Content' => $content,
+                'Request' => null,
+                'Response' => null
+            ];
+        }
+    }
+
+    /**
+     * @return BaseService
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    /**
+     * @return string
+     */
+    public function getData()
+    {
+        return $this->data;
     }
 }
