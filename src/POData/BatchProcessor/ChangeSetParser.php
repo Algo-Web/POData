8cc2067ee415c0bc1a29932487a7a6983134e170
c-harris <c.harris@communicationinnovations.com.au>
2017-12-13 02:11:04 +1000

    Apply fixes from StyleCI (#74)

--- src/POData/BatchProcessor/ChangeSetParser.php
+++ src/POData/BatchProcessor/ChangeSetParser.php
@@ -75,8 +75,8 @@ class ChangeSetParser implements IBatchParser
         $response .= trim($splitter);
         $response .= false === $this->changeSetBoundary ? "\r\n" : "--\r\n";
         $response = 'Content-Length: ' . strlen($response) . "\r\n\r\n" . $response;
-        $response = false === $this->changeSetBoundary ? 
-            $response : 
+        $response = false === $this->changeSetBoundary ?
+            $response :
             'Content-Type: multipart/mixed; boundary=' . $this->changeSetBoundary . "\r\n" . $response;
         return $response;
     }
