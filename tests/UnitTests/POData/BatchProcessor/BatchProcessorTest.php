68bee28309402112e6c33e32f3cfd4b663c3eb4d
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-12-19 04:43:18 +1000

    Unpack array batch-processor payloads

--- tests/UnitTests/POData/BatchProcessor/BatchProcessorTest.php
+++ tests/UnitTests/POData/BatchProcessor/BatchProcessorTest.php
@@ -101,6 +101,85 @@ Host: host
         $foo->handleBatch();
     }
 
+    public function testBatchRequestPartitioningAsArray()
+    {
+        // take the sample batch request in paragraph 2.2 of the OData v3 batch processing docs, feed it into
+        // batchProcessor and verify it chops it into the expected number of things
+
+        $rawData = '
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+Content-Type: application/http 
+Content-Transfer-Encoding:binary
+
+GET /service/Customers(\'ALFKI\') 
+Host: host
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b 
+Content-Type: multipart/mixed; boundary=changeset_77162fcd-b8da-41ac-a9f8-9357efbbd621 
+Content-Length: ###       
+
+--changeset_77162fcd-b8da-41ac-a9f8-9357efbbd621 
+Content-Type: application/http 
+Content-Transfer-Encoding: binary 
+
+POST /service/Customers HTTP/1.1 
+Host: host  
+Content-Type: application/atom+xml;type=entry 
+Content-Length: ### 
+
+<AtomPub representation of a new Customer> 
+
+--changeset_77162fcd-b8da-41ac-a9f8-9357efbbd621 
+Content-Type: application/http 
+Content-Transfer-Encoding:binary 
+
+PUT /service/Customers(\'ALFKI\') HTTP/1.1 
+Host: host 
+Content-Type: application/json 
+If-Match: xxxxx 
+Content-Length: ### 
+
+<JSON representation of Customer ALFKI> 
+
+--changeset_77162fcd-b8da-41ac-a9f8-9357efbbd621-- 
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b 
+Content-Type: application/http 
+Content-Transfer-Encoding:binary 
+
+GET service/Products HTTP/1.1 
+Host: host 
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b--';
+
+        $contentLine = 'multipart/mixed; boundary=batch_36522ad7-fc75-4b56-8c71-56071383e77b';
+        $firstChangeset = m::mock(ChangeSetParser::class);
+        $firstChangeset->shouldReceive('handleData')->andReturnNull()->once();
+        $firstChangeset->shouldReceive('process')->andReturnNull()->once();
+        $firstQuery = m::mock(QueryParser::class);
+        $firstQuery->shouldReceive('handleData')->andReturnNull()->once();
+        $firstQuery->shouldReceive('process')->andReturnNull()->once();
+        $secondQuery = m::mock(QueryParser::class);
+        $secondQuery->shouldReceive('handleData')->andReturnNull()->once();
+        $secondQuery->shouldReceive('process')->andReturnNull()->once();
+
+        $host = m::mock(ServiceHost::class);
+        $host->shouldReceive('getRequestContentType')->andReturn($contentLine)->atLeast(1);
+        $service = m::mock(BaseService::class);
+        $service->shouldReceive('getHost')->andReturn($host)->once();
+        $request = m::mock(RequestDescription::class);
+        $request->shouldReceive('getData')->andReturn([$rawData])->atLeast(1);
+
+        $foo = m::mock(BatchProcessor::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $foo->shouldReceive('getParser')->with($service, m::any(), true)->andReturn($firstChangeset)->once();
+        $foo->shouldReceive('getParser')->with($service, m::any(), false)->andReturn($firstQuery, $secondQuery)
+            ->twice();
+        $foo->shouldReceive('getService')->andReturn($service)->atLeast(1);
+        $foo->shouldReceive('getRequest')->andReturn($request)->atLeast(1);
+
+        $foo->handleBatch();
+    }
+
     public function testGetters()
     {
         $service = m::mock(BaseService::class);
