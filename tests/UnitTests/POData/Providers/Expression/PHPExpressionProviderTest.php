2c3487e0e21032330026862b851af1f2bb32510a
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-12 18:33:31 +1000

    Characterise starts-with handling

--- tests/UnitTests/POData/Providers/Expression/PHPExpressionProviderTest.php
+++ tests/UnitTests/POData/Providers/Expression/PHPExpressionProviderTest.php
@@ -245,6 +245,24 @@ class PHPExpressionProviderTest extends TestCase
         $this->assertEquals($expected, $actual);
     }
 
+    public function testonFunctionCallExpressionStartsWithEmptyNeedle()
+    {
+        $desc = FunctionDescription::filterFunctionDescriptions()["startswith"][0];
+        $prov = new PHPExpressionProvider('foo');
+
+        $issue = $prov->onFunctionCallExpression($desc, ['\'Property\'', '\'\'']);
+
+        $expected = 'strpos(): Empty needle';
+        $actual = null;
+
+        try {
+            eval($issue . ';');
+        } catch (\Exception $e) {
+            $actual = $e->getMessage();
+        }
+        $this->assertEquals($expected, $actual);
+    }
+
     public function testonPropertyAccessExpression()
     {
         $property = m::mock(PropertyAccessExpression::class)->makePartial();
