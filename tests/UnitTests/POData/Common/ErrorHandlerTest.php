fae468abcbde4eef8699a018a6a6533f3d9bb5b8
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-13 21:48:25 +1000

    Fill coverage hole in ErrorHandler

--- tests/UnitTests/POData/Common/ErrorHandlerTest.php
+++ tests/UnitTests/POData/Common/ErrorHandlerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace UnitTests\POData\Common;
 
+use Exception;
 use Mockery as m;
 use POData\Common\ErrorHandler;
 use POData\Common\HttpStatus;
@@ -16,6 +17,10 @@ use POData\OperationContext\ServiceHost;
 use POData\OperationContext\Web\OutgoingResponse;
 use UnitTests\POData\TestCase;
 
+/**
+ * Class ErrorHandlerTest
+ * @package UnitTests\POData\Common
+ */
 class ErrorHandlerTest extends TestCase
 {
     public function testHandleODataException()
@@ -144,6 +149,39 @@ class ErrorHandlerTest extends TestCase
         $expected .= '<error xmlns="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata">' . PHP_EOL;
         $expected .= ' <code>400</code>' . PHP_EOL;
         $expected .= ' <message>Media type requires a \'/\' character.</message>' . PHP_EOL;
+        $expected .= '</error>' . PHP_EOL;
+        $actual = $service->getHost()->getOperationContext()->outgoingResponse()->getStream();
+        $this->assertXmlStringEqualsXmlString($expected, $actual);
+    }
+
+    public function testHandleNonODataExceptionWithValidMimeType()
+    {
+        $exception = new Exception('FAIL', 500);
+
+        $outgoing = m::mock(OutgoingResponse::class);
+        $outgoing->shouldReceive('setServiceVersion')
+            ->withArgs([ODataConstants::DATASERVICEVERSION_1_DOT_0 . ';'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStatusCode')->withArgs(['500 Internal Server Error'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setContentType')->withArgs(['application/xml'])->andReturnNull()->once();
+        $outgoing->shouldReceive('setStream')->passthru();
+        $outgoing->shouldReceive('getStream')->passthru();
+
+        $context = m::mock(IOperationContext::class);
+        $context->shouldReceive('outgoingResponse')->andReturn($outgoing);
+
+        $host = m::mock(ServiceHost::class)->makePartial();
+        $host->shouldReceive('getRequestAccept')->andReturn('application/xml');
+        $host->shouldReceive('getOperationContext')->andReturn($context);
+
+        $service = m::mock(IService::class);
+        $service->shouldReceive('getHost')->andReturn($host);
+
+        ErrorHandler::handleException($exception, $service);
+
+        $expected = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' . PHP_EOL;
+        $expected .= '<error xmlns="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata">' . PHP_EOL;
+        $expected .= ' <code>500</code>' . PHP_EOL;
+        $expected .= ' <message>FAIL</message>' . PHP_EOL;
         $expected .= '</error>' . PHP_EOL;
         $actual = $service->getHost()->getOperationContext()->outgoingResponse()->getStream();
         $this->assertXmlStringEqualsXmlString($expected, $actual);
