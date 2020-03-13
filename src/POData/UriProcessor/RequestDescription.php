c5f486172cc1c9b674be19d7162a6838f96d3c9c
c-harris <c.harris@communicationinnovations.com.au>
2020-03-13 18:24:28 +1000

    Remove JMS - the various readers subsume its functionality

--- src/POData/UriProcessor/RequestDescription.php
+++ src/POData/UriProcessor/RequestDescription.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace POData\UriProcessor;
 
-use JMS\Serializer\SerializerBuilder;
 use POData\Common\Messages;
 use POData\Common\MimeTypes;
 use POData\Common\ODataConstants;
