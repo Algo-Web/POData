3f1228cb5d78bdf7c273fad68b8c200fc3438e15
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-14 19:23:49 +1000

    Fill the worst holes in ODataEntry coverage

--- tests/UnitTests/POData/ObjectModel/ODataEntryTest.php
+++ tests/UnitTests/POData/ObjectModel/ODataEntryTest.php
@@ -6,8 +6,12 @@ namespace UnitTests\POData\ObjectModel;
 
 use AlgoWeb\ODataMetadata\MetadataManager;
 use Mockery as m;
+use POData\Common\ODataConstants;
+use POData\ObjectModel\AtomObjectModel\AtomAuthor;
 use POData\ObjectModel\ODataCategory;
 use POData\ObjectModel\ODataEntry;
+use POData\ObjectModel\ODataLink;
+use POData\ObjectModel\ODataMediaLink;
 use POData\ObjectModel\ODataPropertyContent;
 use POData\Providers\Metadata\ResourceEntityType;
 use POData\Providers\Metadata\ResourceProperty;
@@ -66,5 +70,113 @@ class ODataEntryTest extends TestCase
         $entry = new ODataEntry();
         $entry->setType($category);
         $this->assertEquals('Dice', $entry->resourceSetName);
+    }
+
+    public function testGetAtomContentOfBlankEntry()
+    {
+        $foo = new ODataEntry();
+
+        $res = $foo->getAtomContent();
+        $this->assertEquals('application/xml', $res->type);
+        $this->assertNull($res->src);
+        $this->assertNull($res->properties);
+    }
+
+    public function testGetMediaLink()
+    {
+        $link = m::mock(ODataMediaLink::class)->makePartial();
+        $link->name = 'The Launch';
+
+        $foo = new ODataEntry();
+        $foo->mediaLink = $link;
+
+        $result = $foo->getMediaLink();
+        $this->assertTrue($result instanceof ODataMediaLink);
+        $this->assertEquals('The Launch', $result->name);
+    }
+
+    public function testGetSetLinksBlankArrayRoundTrip()
+    {
+        $foo = new ODataEntry();
+
+        $foo->setLinks([]);
+
+        $expected = [];
+        $actual = $foo->getLinks();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testSetTwoEditLinksLastInBestDressed()
+    {
+        $link1 = new ODataLink('edit', null, null, 'Bitz');
+        $link2 = new ODataLink('edit', null, null, 'Piecez');
+
+        $foo = new ODataEntry();
+
+        $foo->setLinks([$link1, $link2]);
+
+        $editLink = $foo->getEditLink();
+        $this->assertEquals('Piecez', $editLink->url);
+        $this->assertEquals('Piecez', $foo->resourceSetName);
+    }
+
+    public function testFeedInThreeMediaLinks()
+    {
+        $link1 = new ODataMediaLink('edit', null, null, 'Bitz', '', 'edit');
+        $link2 = new ODataMediaLink('edit-media', null, null, 'Bitz', '', 'edit-media');
+        $link3 = new ODataMediaLink('name', null, null, '', '', ODataConstants::ATOM_MEDIA_RESOURCE_RELATION_ATTRIBUTE_VALUE);
+
+        $links = [$link1, $link2, $link3];
+
+        $foo = new ODataEntry();
+        $foo->setMediaLinks($links);
+
+        $mediaLink = $foo->getMediaLink();
+        $this->assertTrue($mediaLink instanceof ODataMediaLink);
+        $this->assertEquals('edit-media', $mediaLink->name);
+
+        $mediaLinks = $foo->getMediaLinks();
+        $this->assertEquals(1, count($mediaLinks));
+        $first = $mediaLinks[0];
+        $this->assertEquals('name', $first->name);
+        $this->assertEquals('/name', $first->srcLink);
+    }
+
+    public function testGetContentWhenNotMediaLinkEntry()
+    {
+        $foo = new ODataEntry();
+
+        $this->assertNull($foo->getPropertyContent());
+    }
+
+    public function testGetContentWhenIsMediaLinkEntry()
+    {
+        $content = new ODataPropertyContent();
+
+        $foo = new ODataEntry();
+        $foo->setPropertyContent($content);
+        $foo->isMediaLinkEntry = true;
+
+        $result = $foo->getPropertyContent();
+        $this->assertTrue($result instanceof ODataPropertyContent);
+        $this->assertEquals(0, count($result->getPropertys()));
+    }
+
+    public function testGetAtomAuthor()
+    {
+        $foo = new ODataEntry();
+
+        $result = $foo->getAtomAuthor();
+
+        $this->assertTrue($result instanceof AtomAuthor);
+        $this->assertEquals('', $result->name);
+    }
+
+    public function testGetType()
+    {
+        $foo = new ODataEntry();
+
+        $this->assertNull($foo->getType());
     }
 }
