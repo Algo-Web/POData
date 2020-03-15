7ee3d82819d1c434b42ea15cf2f368868aebf603
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-15 17:34:08 +1000

    Encapsulate ExpressionTokenId constants

--- src/POData/UriProcessor/QueryProcessor/ExpressionParser/ExpressionTokenId.php
+++ src/POData/UriProcessor/QueryProcessor/ExpressionParser/ExpressionTokenId.php
@@ -34,74 +34,74 @@ use MyCLabs\Enum\Enum;
 class ExpressionTokenId extends Enum
 {
     //Unknown.
-    const UNKNOWN = 1;
+    protected const UNKNOWN = 1;
 
     //End of text.
-    const END = 2;
+    protected const END = 2;
 
     //'=' - equality character.
-    const EQUAL = 3;
+    protected const EQUAL = 3;
 
     //Identifier.
-    const IDENTIFIER = 4;
+    protected const IDENTIFIER = 4;
 
     //NullLiteral.
-    const NULL_LITERAL = 5;
+    protected const NULL_LITERAL = 5;
 
     //BooleanLiteral.
-    const BOOLEAN_LITERAL = 6;
+    protected const BOOLEAN_LITERAL = 6;
 
     //StringLiteral.
-    const STRING_LITERAL = 7;
+    protected const STRING_LITERAL = 7;
 
     //IntegerLiteral. (int32)
-    const INTEGER_LITERAL = 8;
+    protected const INTEGER_LITERAL = 8;
 
     //Int64 literal.
-    const INT64_LITERAL = 9;
+    protected const INT64_LITERAL = 9;
 
     //Single literal. (float)
-    const SINGLE_LITERAL = 10;
+    protected const SINGLE_LITERAL = 10;
 
     //DateTime literal.
-    const DATETIME_LITERAL = 11;
+    protected const DATETIME_LITERAL = 11;
 
     //Decimal literal.
-    const DECIMAL_LITERAL = 12;
+    protected const DECIMAL_LITERAL = 12;
 
     //Double literal.
-    const DOUBLE_LITERAL = 13;
+    protected const DOUBLE_LITERAL = 13;
 
     //GUID literal.
-    const GUID_LITERAL = 14;
+    protected const GUID_LITERAL = 14;
 
     //Binary literal.
-    const BINARY_LITERAL = 15;
+    protected const BINARY_LITERAL = 15;
 
     //Exclamation.
-    const EXCLAMATION = 16;
+    protected const EXCLAMATION = 16;
 
     //OpenParen.
-    const OPENPARAM = 17;
+    protected const OPENPARAM = 17;
 
     //CloseParen.
-    const CLOSEPARAM = 18;
+    protected const CLOSEPARAM = 18;
 
     //Comma.
-    const COMMA = 19;
+    protected const COMMA = 19;
 
     //Minus.
-    const MINUS = 20;
+    protected const MINUS = 20;
 
     //Slash.
-    const SLASH = 21;
+    protected const SLASH = 21;
 
     //Question.
-    const QUESTION = 22;
+    protected const QUESTION = 22;
 
     //Dot.
-    const DOT = 23;
+    protected const DOT = 23;
 
     //Star.
-    const STAR = 24;
+    protected const STAR = 24;
 }
