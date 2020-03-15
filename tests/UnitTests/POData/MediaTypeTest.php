9ca402f305a96a15df2f9f8c4f83fc4bc254ef4d
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-15 12:47:45 +1000

    Fill coverage holes in MediaType

--- tests/UnitTests/POData/MediaTypeTest.php
+++ tests/UnitTests/POData/MediaTypeTest.php
@@ -8,11 +8,33 @@ use Mockery as m;
 use POData\MediaType;
 use UnitTests\POData\TestCase;
 
+/**
+ * Class MediaTypeTest
+ * @package UnitTests\POData
+ */
 class MediaTypeTest extends TestCase
 {
     public function testGetParameters()
     {
         $foo = new MediaType('foo', 'bar', []);
         $this->assertEquals(0, count($foo->getParameters()));
+    }
+
+    public function testGetMatchingRatingOnUniversalMatch()
+    {
+        $foo = new MediaType('*', '*', []);
+
+        $expected = 0;
+        $actual = $foo->getMatchingRating('application/json');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetMatchingRatingOnUniversalSubtypeMatch()
+    {
+        $foo = new MediaType('application', '*', []);
+
+        $expected = 1;
+        $actual = $foo->getMatchingRating('application/json');
+        $this->assertEquals($expected, $actual);
     }
 }
