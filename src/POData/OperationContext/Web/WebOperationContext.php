87f86554ccabee068530a929ed553c3d68aab488
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-05-06 22:06:22 +1000

    Let incoming request and outgoing response be supplied to WebOperationContext

--- src/POData/OperationContext/Web/WebOperationContext.php
+++ src/POData/OperationContext/Web/WebOperationContext.php
@@ -37,11 +37,13 @@ class WebOperationContext implements IOperationContext
      * This function will perform the following tasks:
      *  (1) Retrieve the current HTTP method,headers and stream.
      *  (2) Populate $_incomingRequest using these.
+     * @param IHTTPRequest|null $incomingRequest
+     * @param OutgoingResponse|null $outgoingResponse
      */
-    public function __construct()
+    public function __construct(IHTTPRequest $incomingRequest = null, OutgoingResponse $outgoingResponse = null)
     {
-        $this->incomingRequest  = new IncomingRequest();
-        $this->outgoingResponse = new OutgoingResponse();
+        $this->incomingRequest  = $incomingRequest ?? new IncomingRequest();
+        $this->outgoingResponse = $outgoingResponse ?? new OutgoingResponse();
     }
 
     /**
