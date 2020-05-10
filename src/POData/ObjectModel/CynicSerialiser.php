c040a992032816737378c9a8b8cc467980f5a126
c-harris <c.harris@communicationinnovations.com.au>
2020-05-10 15:05:41 +1000

    reorder for simplicity

--- src/POData/ObjectModel/CynicSerialiser.php
+++ src/POData/ObjectModel/CynicSerialiser.php
@@ -168,10 +168,8 @@ class CynicSerialiser implements IObjectSerialiser
         }
 
         $resourceSet = $this->getRequest()->getTargetResourceSetWrapper()->getResourceSet();
-        $requestTop  = $this->getRequest()->getTopOptionCount();
         $pageSize    = $this->getService()->getConfiguration()->getEntitySetPageSize($resourceSet);
-        $requestTop  = (null === $requestTop) ? $pageSize + 1 : $requestTop;
-
+        $requestTop  = $this->getRequest()->getTopOptionCount() ?? $pageSize + 1;
         if (true === $entryObjects->hasMore && $requestTop > $pageSize) {
             $lastObject          = end($entryObjects->results);
             $odata->nextPageLink            = new ODataLink(
