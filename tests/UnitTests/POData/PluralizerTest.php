4c2282d5d9ae333258b39e9f7a870348728f81e4
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-05-15 17:58:31 +1000

    Wring out Pluralizer to finish regaining coverage loss

---
+++ tests/UnitTests/POData/PluralizerTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 15/05/20
+ * Time: 5:33 PM
+ */
+
+namespace UnitTests\POData;
+
+use POData\Pluralizer;
+
+/**
+ * Class PluralizerTest
+ * @package UnitTests\POData
+ */
+class PluralizerTest extends TestCase
+{
+    public function testPluraliseSingular()
+    {
+        $single = 'single';
+
+        $expected = 'single';
+
+        $actual = Pluralizer::plural($single, 1);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testPluraliseUncountable()
+    {
+        $single = 'sheep';
+
+        $expected = 'sheep';
+
+        $actual = Pluralizer::plural($single, 2);
+        $this->assertEquals($expected, $actual);
+    }
+}
